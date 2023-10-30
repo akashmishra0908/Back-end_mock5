79b2285dfc744e74ebdee48cd9cda7880a1198fa
akashmishra0908 <112822976+akashmishra0908@users.noreply.github.com>
2023-10-30 17:55:25 +0530

    back-end

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const express=require("express");
 const app=express();
 const { connection} =require("./db");
 
-// app.listen(process.env.PORT,async()=>{
-//     try {
-//         await connection
-//         console.log("server is running ");
-//     } catch (error) {
-//         console.log(error)
-//     }
-// })
-module.exports=app
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection
+        console.log("server is running ");
+    } catch (error) {
+        console.log(error)
+    }
+})
+// module.exports=app
