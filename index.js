cfd46e3723eafd016fec28e564f72251a508bd7c
akashmishra0908 <112822976+akashmishra0908@users.noreply.github.com>
2023-10-30 17:08:58 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose");
+const Cors=require("cors");
+
+const express=require("express");
+
+const app=express();
+const { connection} =require("./db");
+
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection
+        console.log("server is running ");
+    } catch (error) {
+        console.log(error)
+    }
+})
