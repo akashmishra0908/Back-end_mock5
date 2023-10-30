80dcad19570f2a02d5517754693a7bc5178d34c4
akashmishra0908 <112822976+akashmishra0908@users.noreply.github.com>
2023-10-30 17:36:48 +0530

    login

--- controller/userController.js
+++ controller/userController.js
@@ -24,6 +24,29 @@ const register = async (req, res) => {
     }
 }
 
+const login=()=>async(req,res)=>{
+try {
+    const { email, password } = req.body;
+    let alrdyUser=await User.findOne({email:email});
 
+    if(!alrdyUser){
+        res.status(201).send("user does not exist");
+        return;
+    }
+    let passwodConfirmation=await bcrypt.compare(password,alrdyUser.password);
+    if(!passwodConfirmation){
+        res.status(202).send("Invalid Password...");
+        return;
+    }
+    else {
+        let token=jwt.sign({userId:alrdyUser._id}.jwtsecret);
+        res.status(200).send({token:token,user:alrdyUser})
+    }
+    res.status(200).send(createdUser);
+} catch (error) {
+    res.status(400).send(err);
+    
+}
+}
 
 module.exports={register}
